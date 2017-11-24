93a8683408d9b9f4e10cd720b5a395a1102c251c
kanghua <kanghua151@gmail.com>
2017-11-24 16:04:46 +0800

    .....

--- gbroke.py
+++ gbroke.py
@@ -546,7 +546,7 @@ class GBroke:
             def __init__(self,context,url,products):
                 super(WSClient, self).__init__(url = url,
                                                product_id = products,
-                                               auth = True,
+                                               auth = False,
                                                api_key = APK_KEY,
                                                api_secret = API_SECRET,
                                                api_passphrase = API_PASSPHRASE
@@ -1151,10 +1151,10 @@ class GBroke:
     #
     #     self._call_tick_handlers(msg.tickerId, acc.peek())
     def _received(self, msg):
-        print("profile_id:",self.profile_id,msg['profile_id'],msg['client_oid'],type(msg['profile_id']),type(self.profile_id))
+        #print("profile_id:",self.profile_id,msg['profile_id'],msg['client_oid'],type(msg['profile_id']),type(self.profile_id))
         if 'profile_id' in msg and msg['profile_id'] == self.profile_id:
             self.log.info('my order .....%s',msg)
-            order = self._orders.get(msg['client_oid'])
+            order = self._orders.get(msg['client_oid'] if 'client_oid' in msg['client_oid'] else msg['order_id'] )
             if not order:
                 self.log.warning('Manual ORDER Received %s', msg['order_id'])
                 instrument = self._instruments.get(msg['product_id'])

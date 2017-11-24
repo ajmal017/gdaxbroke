5031e203de639f080310735fa939ae6ea91de8b4
kanghua <kanghua151@gmail.com>
2017-11-24 16:47:51 +0800

    .....

--- gbroke.py
+++ gbroke.py
@@ -1154,7 +1154,12 @@ class GBroke:
         #print("profile_id:",self.profile_id,msg['profile_id'],msg['client_oid'],type(msg['profile_id']),type(self.profile_id))
         #if 'profile_id' in msg and msg['profile_id'] == self.profile_id:
         self.log.info('my order .....%s',msg)
-        order = self._orders.get(msg['client_oid'] if 'client_oid' in msg['client_oid'] else msg['order_id'] )
+        if 'client_oid' in msg['client_oid']:
+            oid = msg['client_oid']
+        else:
+            oid = msg['order_id']
+        print(oid)
+        order = self._orders.get(oid)
         if not order:
             self.log.warning('Manual ORDER Received %s', msg['order_id'])
             instrument = self._instruments.get(msg['product_id'])
